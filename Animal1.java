08363036bc5510496b67263dc8166ccf63c79e71
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-15 13:02:20 +0530

    Animal

---
+++ Animal1.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class Animal {
+	
+	public void sound() {
+		System.out.println("Animal Make Sound !!");
+	}
+	
+	public void eat() {
+		System.out.println("Animal Eats");
+	}
+
+}
