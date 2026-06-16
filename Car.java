35c6dfaa096101a692c3f9d2ee8b24c792f48023
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-16 11:52:12 +0530

    inheritance property using oops

---
+++ Car.java
@@ -0,0 +1,24 @@
+package oops;
+
+public class Car {
+	
+	// state ***********
+	
+	int model_no;
+	String name;
+	double price;
+	
+	
+	// Behavior **********
+	
+	public void start() {
+		System.out.println("Start The Car");
+	}
+	
+	public void stop() {
+		System.out.println("Stop The Car");
+	}
+	
+	
+
+}
