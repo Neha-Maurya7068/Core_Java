2ee40c2b6f30ef7048681dc62107a6a21c54fb62
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-16 12:00:42 +0530

    Concatination

---
+++ ConcatinationEven.java
@@ -0,0 +1,19 @@
+  public class ConcatinationEven{
+  
+  public static void main(String[]args){
+
+   int n = 10;
+
+  if(n % 2 == 0 ){
+  
+  System.out.println(n+ ":Is Even");
+   
+  }
+  else{
+   
+  System.out.println(n+ ":Is Odd");
+  
+  }
+  }
+  }
+  
