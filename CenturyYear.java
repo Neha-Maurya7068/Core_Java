ad730aa61e006e354099b7601ec04466a10d067d
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-16 11:55:08 +0530

    Century year using ternary operator

---
+++ CenturyYear.java
@@ -0,0 +1,32 @@
+  public class CenturyYear{
+  
+ public static void main(String[]args){
+
+  int year = 1700;
+
+ 
+  if(year % 100 == 0){ 
+  
+  System.out.println("Century Year");
+
+  }
+ else{
+
+ System.out.println("Not Century Year");
+
+  }
+ if(year % 4 ==0){
+   
+  System.out.println("Leap Year");
+
+  }
+
+  else{
+  
+   System.out.println(" Not Leap Year");
+  
+  }
+
+ }
+
+ }
