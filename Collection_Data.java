1cff85c00aee6d0883bb4e3a305eeb855d15008d
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-16 11:59:38 +0530

    Add files via upload

---
+++ Collection_Data.java
@@ -0,0 +1,82 @@
+package jcf;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public class Collection_Data {
+
+	public static void main(String[] args) {
+
+//		Collection<String> c1 = new ArrayList<>();
+//
+//		c1.add("Ram");
+//		c1.add("Radha");
+//		c1.add("Sushil");
+//		c1.add("Akansha");
+//		c1.add("Neha");
+//		c1.add("Shivam");
+//		c1.add("Manish");
+//		c1.add("Deepanshu");
+//		c1.add("Khushboo");
+//		c1.add("Muheeb");
+//
+//		System.out.println(c1);
+//		
+//		c1.remove("Muheeb");
+//		
+//		System.out.println(c1);
+
+//		System.out.println(c1.size());
+
+//		System.out.println(c1.contains("Neha"));
+
+//		c1.clear();
+
+//		System.out.println(c1.isEmpty());
+
+//		for(String str : c1) {
+//			System.out.println(str);
+//	
+//		}
+		
+//		*****************************************************************
+
+		Collection<Integer> l1 = new ArrayList<>();
+
+		l1.add(1);
+		l1.add(2);
+		l1.add(3);
+		l1.add(4);
+		l1.add(5);
+
+//		Iterator<Integer> it = l1.iterator();
+//
+//		while (it.hasNext()) {
+//
+//			int result = it.next();
+//
+//			if (result % 2 == 0) {
+//
+//				it.remove();
+//			}
+//		}
+//		System.out.println(l1);
+		
+		
+
+		
+//		for(Integer i : l1) {
+//			if(i % 2 == 0) {
+////				System.out.println(i);
+//				l1.remove(i);  // ❌ Exception
+//			}
+//		}
+//
+
+
+	}
+
+}
