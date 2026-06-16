7d2c8dd37a089ea8e35a44acc1eaaa3766d55c9b
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-16 12:01:30 +0530

    Conditional Statement

---
+++ ConditionalStatement.java
@@ -0,0 +1,21 @@
+ public class ConditionalStatement{
+  
+ public static void main(String[]args){
+   
+  // If Statement********
+
+  int age = 16;
+ 
+  if(age >= 18){
+  
+  System.out.println("Adult");
+  }
+  else{
+
+  System.out.println("Minor");
+  
+  }
+
+ }
+
+ }
