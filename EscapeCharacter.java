f6b6a6476de08770a2451279dd8d688f634ab12e
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-17 06:31:10 +0530

    Escape Character

---
+++ EscapeCharacter.java
@@ -0,0 +1,79 @@
+package escape_character;
+
+//import java.io.File;
+//import java.io.IOException;
+
+public class EscapeCharacter {
+
+	// public static void main(String[] args) throws IOException {
+		
+	public static void main(String[] args) {
+	
+		
+	
+			//  Neha is a student of core java
+		
+	//	System.out.println("Neha is a student of core java");
+		
+		//  Neha is a "student" of core java
+		
+//		 System.out.println("Neha is a \"student\" of core java");
+		
+		
+	//  Neha is a \student\ of core java
+		
+//		System.out.println("Neha is a \\student\\ of core java");
+		
+//		System.out.println("'");
+		
+//		System.out.println('\'');
+		
+//		System.out.println("Today Is \n Wednesday");
+		
+//		System.out.println("Today Is \t Wednesday");
+		
+		
+//		System.out.println("Hello \b world");
+//		
+//		File f = new File("E:\\CoreJava\\JavaAt 02\\helloStudents.pdf");
+//		
+//		if(f.createNewFile()) {
+//			System.out.println("File Created Sucessfully !!");
+//		}
+//		else {
+//			System.out.println("File Cannot Created !!");
+//		}
+
+		/*
+		 *       /  -> Forward Slash
+		 *       \  -> Backward Slash
+		 */
+		
+		// Comments
+		
+		// single Line :-
+		
+		// Single Line Comments
+		
+		// MultiLine :-
+		
+		/*
+		 * Multiline Comments
+		 * 
+		 * here we can write Multiple lines
+		 */
+		
+		// Descriptive :-
+		
+		/**
+		 * @author vivek pandey
+		 * date :- 17 December 2025
+		 * 
+		 * This is Descriptive comment
+		 * 
+		 * here also we can write Multiple lines
+		 */
+		
+	}
+
+}
