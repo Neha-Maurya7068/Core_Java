f0a4471b162aaf5cff61f611fde64d2c445e3113
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-17 06:14:49 +0530

    Demo is the first class core java

---
+++ Demo.java
@@ -0,0 +1,8 @@
+public class Demo {
+    static char ch; // default value
+
+    public static void main(String[] args) {
+        System.out.println("Default value of char: [" + ch + "]");
+        System.out.println("Unicode value: " + (int) ch ); //
+    }
+}
