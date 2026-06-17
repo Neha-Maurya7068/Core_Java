858544265bf302b68aeb7ff7cb2f2693e58041d8
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-17 06:32:05 +0530

    9th commit

---
+++ EscapeCharacter1.java
@@ -0,0 +1,68 @@
+public class EscapeCharacter {
+
+	public static void main(String[] args) {
+
+	/*
+
+//	System.out.println("Neha is a student of core java");
+		
+		//  Neha is a "student" of core java
+		
+//		 System.out.println("Neha is a \"student\" of core java");
+		
+		
+	//  Neha is a \student\ of core java
+		
+		System.out.println("Neha is a \\student\\ of core java");
+		
+	//	System.out.println("'");
+		
+		System.out.println('\'');
+		
+		System.out.println("Today Is \n Wednesday");
+		
+		System.out.println("Today Is \t Wednesday");
+		*/
+		
+	// System.out.println("Hello \bworld");
+
+	// System.out.println("Hello\b world");
+
+	// System.out.println("Hello \rhii");
+			
+//	System.out.println("Today Is \f Wednesday");
+
+	// Unicode **
+
+	// System.out.println("Hello");
+
+	// System.out.println('Hello'); // not Valid
+		
+	// System.out.println('H');
+
+	// System.out.println('\u0046');
+
+/*
+	System.out.println('\u004A');
+	System.out.println('\u0061');
+	System.out.println('\u0076');
+	System.out.println('\u0061');
+
+
+
+System.out.println("\u004A\u0061\u0076\u0061");
+
+
+
+System.out.println("Neha is a 'student' of core java");
+
+System.out.println("Neha is a \'student\' of core java");
+
+*/
+
+
+
+
+	}
+
+}
