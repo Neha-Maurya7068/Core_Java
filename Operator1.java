a47eb18dc36d18791409d4428f67f468122e6420
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-18 14:13:11 +0530

    2nd commit

---
+++ Operator1.java
@@ -0,0 +1,117 @@
+class Operator{
+
+	public static void main(String[] args){
+
+	/*
+			int a = 10;
+			int b = 2;
+
+			int sum = a + b;
+			int sub = a - b;
+			int prod = a * b;
+			int div = a / b;
+			int mod = a % b;
+
+		System.out.println("Aritimetic Operator");
+
+		System.out.println(sum);
+		System.out.println(sub);
+		System.out.println(prod);
+		System.out.println(div);
+		System.out.println(mod);
+
+	
+		
+		System.out.println("Relational Operator");
+
+		System.out.println(10 > 20);
+		System.out.println(10 < 20);
+		System.out.println(10 == 20);
+		System.out.println(10 >= 20);
+		System.out.println(10 <= 20);
+		System.out.println(10 != 20);
+		
+	
+
+		System.out.println("Logical Operator");
+		// AND && **
+
+		System.out.println(true && true);
+		System.out.println(true && false);
+		System.out.println(false && true);
+		System.out.println(false && false);
+		// OR || **
+
+		System.out.println(true || true);
+		System.out.println(true || false);
+		System.out.println(false || true);
+		System.out.println(false || false);
+
+	
+		// NOT !
+		
+		System.out.println(!true);
+		System.out.println(!false);
+
+	
+	System.out.println("Assignment Operator");
+
+		int a = 10;
+
+		  a += 20;
+
+	   System.out.println(a);
+
+	
+
+	System.out.println("BitWise Operator");
+
+	// & , | , ~ , ^ , >> , << 
+
+	// a / 2^b = right
+	// a * 2^b = left
+	// -( a + 1) = not
+
+	
+	 System.out.println(5 & 1);
+ 	 System.out.println(7 & 1);
+
+
+	 System.out.println(5 | 1);
+ 	 System.out.println(7 | 1);
+
+	System.out.println(8 & 1); 
+
+	
+	 System.out.println(5 ^ 1);
+ 	 System.out.println(7 ^ 1);
+
+
+	
+
+	 System.out.println(5 >> 1);
+ 	 System.out.println(7 >> 1);
+	
+	 System.out.println(5 << 1);
+ 	 System.out.println(7 << 1);
+
+	
+	
+	 System.out.println(~5);
+
+ 	 System.out.println(~7);
+
+	*/
+	
+
+
+	
+
+
+	
+	
+		
+
+
+	}
+}
