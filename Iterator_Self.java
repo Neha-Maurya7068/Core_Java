796ee2ca5e73efaf5d3174e55a77662d2fd68931
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-18 13:52:08 +0530

    self iterator

---
+++ Iterator_Self.java
@@ -0,0 +1,58 @@
+package jcf;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public class Iterator_Self {
+
+	public static void main(String[] args) {
+
+//		Collection<Integer> c1 = new ArrayList<>();
+//		
+//		c1.add(10);
+//		c1.add(20);
+//		c1.add(30);
+//		c1.add(40);
+//		c1.add(50);
+
+//		System.out.println(c1);
+//		
+//		System.out.println(c1.size());
+
+		// For Each *******
+
+//		for(Integer i : c1) {
+//			System.out.println(i);
+//		}
+
+//		List<String> l1 = Arrays.asList("Mohan", "Rohan", "Ram");
+//
+//		Iterator<String> it = l1.iterator();
+//
+//		while (it.hasNext()) {
+//			System.out.println(it.next());
+//		}
+
+//		Print 5 Numbers Using Iterator *****
+//      Print Even Number From List Using Iterator *******
+
+//		List<Integer> l1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+//
+//		Iterator<Integer> it = l1.iterator();
+//
+//		while (it.hasNext()) {
+//
+//			int result = it.next();
+//
+//			if (result % 2 == 0) {
+//
+//				System.out.println(result);
+//			}
+//		}
+
+	}
+
+}
