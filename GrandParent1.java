7e7949947bf97e17bf1389e39ed1b64564b2a582
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-18 13:48:50 +0530

    Add files via upload

---
+++ GrandParent1.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class GrandParent {
+	
+	int a = 10;
+	
+	public void getData() {
+		System.out.println("This Is A GrandParent GetData Method !!");
+		System.out.println("From Grand Parent Value Of A : "+a);
+	}
+
+}
