8071cbce7965c80aa7f1452367b0c3e00a214cc0
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-18 14:13:59 +0530

    3rd commit

---
+++ Operator2.java
@@ -0,0 +1,165 @@
+class Operator{
+	
+	public static void main(String[] args){
+
+/*
+		System.out.println("Arithematic Operator");
+
+	System.out.println(12+2);
+	System.out.println(12-2);
+	System.out.println(12*2);
+	System.out.println(13%2);
+	System.out.println(12/2);
+	
+
+
+	System.out.println("RELATIONAL Operator");
+
+	System.out.println(12 > 2);
+	System.out.println(12 < 2);
+	System.out.println(12 >= 2);
+	System.out.println(13 <= 2);
+	System.out.println(12 == 2);
+	System.out.println(12 != 2);
+
+
+
+	System.out.println("Logical Operator");
+
+	//AND &&
+
+	System.out.println(true && true);
+	System.out.println(true && false);
+	System.out.println(false && true);
+	System.out.println(false && false);
+
+	//OR ||
+
+	System.out.println(true || true);
+	System.out.println(true || false);
+	System.out.println(false || true);
+	System.out.println(false || false);
+
+
+
+	//NOT !
+
+	System.out.println(!true);
+	System.out.println(!false);
+
+
+
+	System.out.println("Assignment Operator");
+
+	int a = 10;
+
+	// a = a+5;
+
+	// a += 5;
+
+	a *= 5;
+
+	System.out.println(a);
+
+
+
+
+	// Unary ******
+
+	// ++ , --
+
+	// a++ , ++a
+
+	// int a = 20;
+
+	// System.out.println(a++); // 20  -> 21
+	// System.out.println(++a); // 22
+	
+		//a++;
+
+	//System.out.println(a); // 21
+
+		int a = 20;
+
+		//a = a++;
+
+		int c = a++;
+
+	System.out.println(a);
+	System.out.println(c);
+*/
+
+/*
+	int x = 23;
+	x++;
+
+	int y = x++;
+
+	int z = ++y;
+
+	System.out.println(x);
+	System.out.println(y);
+	System.out.println(z);
+
+
+*/
+
+/*
+	int a = 17;
+
+	a = a++;
+	a = a++;
+	a = a++;
+
+	a = a--;
+	a = a--;
+	a = a++;
+	
+	a = a--;
+	a = a--;
+
+	a = a++;
+
+	System.out.println(a);
+
+
+	int a = 2;
+
+	int sum = a++ + a++ + ++a;
+
+	System.out.println(sum);
+
+*/	
+
+/*
+	
+	int age = 15;
+
+String res = (age >= 18)? "Can Vote ": "Can't Vote";
+
+	System.out.println(res);	
+	
+
+	// odd even
+
+	int num = 14;
+
+	String res = (num % 2 == 0)? "Even ": "Odd";
+
+	System.out.println(res);
+*/
+	// driver can drive 18 to 55
+
+	int age = 24;
+
+String res = (age >= 18 && age <= 55 ) ? "Can Drive ": "Can't Drive";
+
+	System.out.println(res);
+
+	
+
+
+
+	}
+
+}
