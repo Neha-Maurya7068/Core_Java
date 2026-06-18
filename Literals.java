03215c638f31e1231e9bb308e5fccb6d68a1f725
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-18 13:57:18 +0530

    Literals basic core java

---
+++ Literals.java
@@ -0,0 +1,67 @@
+class Literals {
+
+	public static void main(String[] args){
+
+/*
+		int a = 10;
+		double d = 12.7;
+		String str = "Abhinav";
+		int[] arr = {10, 20, 30, 40, 50};
+		String s = null
+
+
+
+	System.out.println(a);
+	System.out.println(str);
+	System.out.println(d);
+	System.out.println(arr[0]);  //  10
+	System.out.println(arr[4]);  // 
+	System.out.println(arr.length); // 5
+
+	System.out.println(s);
+	System.out.println(s.length());
+
+*/
+
+	//	int a = 30;
+	//	int b = 60;
+
+	// System.out.println(a+b+" Hello Students");
+
+	//System.out.println("Hello Students "+a+b);
+
+//System.out.println(a+b+" Hello Students "+a+b);
+
+	//System.out.println(b-a+" Hello Students ");
+
+	//System.out.println(" Hello Students "+b-a);
+
+	//System.out.println(a*b+" Hello Students ");
+
+	//System.out.println("Hello Students "+a*b);
+
+	//System.out.println("Hello Students "+(b-a));
+
+	//String a = "Abhinav";
+	//String b = "Aditya";
+
+	//System.out.println(a+b);
+
+
+	// float f = 12.5F;
+	//long l1 = 12l;
+
+	//int l1 = 12l;
+
+	byte b1 = 127;
+
+	//System.out.println(f);
+
+	//System.out.println(l1);
+
+	System.out.println(b1);
+
+
+	}
+
+}
