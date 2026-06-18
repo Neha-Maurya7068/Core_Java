b5c80a5f7ae278f2b39f63d41582db370c188957
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-18 13:49:27 +0530

    Input method

---
+++ Input.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Input {
+	public static void main(String [] args){
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter Data : ");
+
+		// byte n = sc.nextByte();
+		// short n = sc.nextShort();
+		// int n = sc.nextInt();
+		// long n = sc.nextLong();
+
+		// double n = sc.nextDouble();
+		// float n = sc.nextFloat();
+
+		// boolean n = true;
+
+		// String n = sc.next();
+		// String n = sc.nextLine();
+
+		// char n = sc.next().charAt(3);
+
+		char n = sc.next().charAt(0);
+
+		System.out.println("You Have Entered : "+n);
+		
+
+
+	}
+
+}
