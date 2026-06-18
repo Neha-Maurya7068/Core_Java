3279f0cded135bc9670281c8428935ffa7f63cbc
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-18 14:04:10 +0530

    Modifiers Driver

---
+++ ModifiersDriver.java
@@ -0,0 +1,16 @@
+package access_modifires;
+
+public class ModifiersDriver {
+	
+	public static void main(String[] args) {
+		
+		Modifires m1 = new Modifires();
+		
+		System.out.println("Public A : "+m1.a);
+		System.out.println("Protected B : "+m1.b);
+		System.out.println("Defult C : "+m1.c);
+//		System.out.println("Private D : "+m1.d);
+		
+	}
+
+}
