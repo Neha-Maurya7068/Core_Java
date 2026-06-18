ccbf2aa9a0e8fcafc1c8b79f92d4c9c040219b3e
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-18 14:11:43 +0530

    Number System

---
+++ NumberSystem.java
@@ -0,0 +1,13 @@
+class NumberSystem{
+
+	public static void main(String[] args){
+
+		System.out.println("Decimal : "+45); // 45
+		System.out.println("Binary : "+0b101101);  // 45
+		System.out.println("Octal : "+045);  // 37
+		System.out.println("HexaDecimal : "+0x45);  // 69
+	
+	}
+
+
+}
