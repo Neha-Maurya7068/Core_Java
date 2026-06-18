0d6458fc61b661a446219dac96e995cf0f094bc9
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-18 14:01:22 +0530

    Add files via upload

---
+++ Method_1.java
@@ -0,0 +1,20 @@
+package Methods;
+
+public class Method_1 {
+
+	public static void main(String[] args) {
+		
+//		Method_Intro.printName("Dipendra");
+		
+//		Method_Intro.getSum();
+		
+		
+//		Method_Intro m1 = new Method_Intro();
+//		m1.getSum(44, 60);
+//		
+//		m1.printName("Mohan");
+		
+//		System.out.println(Question.getFactorial(7));
+		
+	}
+}
