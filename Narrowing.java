0d795fa29c727d1af428dea21bba50e8d30817fa
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-18 14:11:06 +0530

    Narrowing class

---
+++ Narrowing.java
@@ -0,0 +1,42 @@
+package typecasting;
+
+public class Narrowing {
+	
+	// Explicit
+	// Manually
+	// type caste operator
+	// data loss happen
+
+	public static void main(String[] args) {
+		
+//		double a = 20.65;
+//		System.out.println(a);
+		
+//		int b = (int)a;
+//		
+//		System.out.println(b);
+		
+		
+//		int a = 128;
+		
+//		System.out.println(a);
+		
+//		byte b = (byte)a;
+//		
+//		System.out.println(b);
+		
+		
+//		int a = 50;
+		
+//		System.out.println(a);
+		
+//		char ch = (char)a;
+		
+//		char ch = 'Z';
+//		
+//		int a = ch ;
+//		
+//		System.out.println(a);
+		
+	}
+}
