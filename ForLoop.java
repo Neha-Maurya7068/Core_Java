23553d571de54ed264c6ee6a27eacfab7a53b341
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-18 13:47:12 +0530

    for loop

---
+++ ForLoop.java
@@ -0,0 +1,61 @@
+package loop;
+
+import java.util.Scanner;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Number !!");
+		int n = sc.nextInt();
+		
+//		System.out.println("Hello Java");
+//		System.out.println("Hello Java");
+//		System.out.println("Hello Java");
+//		System.out.println("Hello Java");
+//		System.out.println("Hello Java");
+		
+		/* Print Natural Number */
+		
+//		for(int i = 1; i <= 100; i++) {
+//			System.out.println("Hello Neha "+i);
+//		}
+		
+		
+		/* Sum Of Natural Number */
+		
+//		  int sum = 0;
+//		
+//		for(int i = 1; i <= n; i++) {
+//			
+//			sum = sum + i;
+//			
+//		}
+//		
+//		System.out.println("Sum Of "+n+" Natural Number Is : "+sum);
+		
+		
+		/* Factorial Of  Number */
+		
+		
+//		 int fact = 1;
+//			
+//			for(int i = 1; i <= n; i++) {
+//				
+//				fact = fact * i;
+//				
+//			}
+//			
+//			System.out.println("Factorial Of "+n+" Is : "+fact);
+		
+		/* Print Table Of Given Number */
+		
+//		for(int i = 1; i <= 10; i++) {
+//			System.out.println(n +" X "+ i + " = "+ n*i );
+//		}
+//		
+		
+	}
+}
