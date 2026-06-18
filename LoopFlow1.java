7699610af15dc1b2ae506a0a7767779718b4c938
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-18 13:58:03 +0530

    Loop Flow

---
+++ LoopFlow1.java
@@ -0,0 +1,40 @@
+package loop;
+
+public class LoopFlow {
+
+	public static void main(String[] args) {
+
+//		for(int i = 1; i <= 10; i++) {
+//			System.out.println(i);
+//		}
+
+//		Break *************
+
+//		for (int i = 1; i <= 10; i++) {
+//
+//			System.out.println(i);
+//			if (i == 5) {
+//				break;
+//			}
+//		}
+//
+//		System.out.println("Break Statement");
+
+		// Continue ******************
+
+//		for (int i = 1; i <= 10; i++) {
+//
+//			if (i == 5) {
+//				continue;
+//			}
+//			System.out.println(i);
+//		}
+//
+//		System.out.println("Continue Statement");
+		
+
+		
+		
+		
+	}
+}
