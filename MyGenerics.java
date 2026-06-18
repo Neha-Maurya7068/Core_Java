074c61afbe6dbe6a04b088403aa10d947a91a72f
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-18 14:06:57 +0530

    My Generics in jcf

---
+++ MyGenerics.java
@@ -0,0 +1,26 @@
+gtpackage generics;
+
+public class MyGenerics {
+
+//	
+//	Object obj;
+//	
+//	MyGenerics(Object obj){
+//		this.obj = obj;
+//	}
+
+//	void display() {
+//		System.out.println("Object Is : "+obj);
+//	}
+
+	private Object obj;
+
+	public Object getValue() {
+		return obj;
+	}
+
+	public void setValue(Object obj) {
+		this.obj = obj;
+	}
+
+}
