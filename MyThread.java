f6c22f5f8494eb2e6ded8e085d4f7c8c334a2c8f
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-18 14:10:07 +0530

    Thread

---
+++ MyThread.java
@@ -0,0 +1,57 @@
+package multithreading;
+
+//import java.util.Scanner;
+
+public class MyThread extends Thread {
+
+	@Override
+	public void run() {
+		
+		
+//		System.out.println(getName());
+	
+		
+//		for(int i = 1; i <= 3; i++){
+//			
+//			try {
+//				sleep(1000);
+//				System.out.println(currentThread().getName()+" ----> "+i);
+//			} catch (InterruptedException e) {
+//				
+//				e.printStackTrace();
+//			}
+//			
+//		}
+		
+		
+		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter Number For Table : ");
+//		int n = sc.nextInt();
+//		for(int i = 1; i <= 10; i++) {
+//			try {
+//				sleep(2000);
+//				System.out.println(n+" X "+i+" = "+n*i);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//			
+//		}
+	
+		
+		
+//		System.out.print("Loading");
+//		for(int i = 1; i <= 5; i++) {
+//		try {
+//			sleep(1000);
+//			System.out.print(".");
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		}
+//		System.out.println("\nLoading Completed");
+		
+		
+		
+	}
+}
