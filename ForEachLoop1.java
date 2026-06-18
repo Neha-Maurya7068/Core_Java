a0031bfc37fbb199e35011a1cf2bf0ec260e7764
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-18 13:46:12 +0530

    For each loop

---
+++ ForEachLoop1.java
@@ -0,0 +1,44 @@
+package loop;
+
+public class ForEachLoop {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * for(Data_type var_name : collections){
+		 * statements;
+		 * }
+		 * 
+		 */
+		
+//		int[] arr = {10, 20, 30, 40, 50};
+		
+//		for(int i = 0; i < arr.length; i++) {
+//			System.out.println(arr[i]);
+//		}
+		
+//		int sum = 0;
+//		for(int i = 0; i < arr.length; i++) {
+//			sum = sum + arr[i];
+//		}
+//		System.out.println("Sum Of Element : "+sum);
+		
+//		
+//		int[] arr = {10, 20, 30, 40, 50};
+//		
+//		
+////		for(int i : arr) {
+////			System.out.println(i);
+////		}
+//		
+//		int sum = 0;
+//		for(int i : arr) {
+//			sum = sum + i;
+//		}
+//		System.out.println("Sum Of Element : "+sum);
+		
+		
+		
+	}
+
+}
