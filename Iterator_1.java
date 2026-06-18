eec2e767f62c89668355466eec3bb83284d40fc1
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-18 13:50:46 +0530

    iterator

---
+++ Iterator_1.java
@@ -0,0 +1,31 @@
+package jcf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Iterator_1 {
+
+	public static void main(String[] args) {
+		
+		List<Integer> l1 = new ArrayList<>();
+		
+		l1.add(10);
+		l1.add(20);
+		l1.add(30);
+		l1.add(40);
+		l1.add(50);
+		
+//		System.out.println(l1.size());
+		
+//		System.out.println(l1);
+		
+		
+		// For Each ***********
+		
+//		for(Integer i : l1) {
+//			System.out.println(i);
+//		}
+		
+	}
+
+}
