58faae8a78f5f2c4344a8544acb9cbbeedc21b07
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-19 23:24:25 +0530

    3rd commit

---
+++ ParentChildDriver1.java
@@ -0,0 +1,29 @@
+package inheritance;
+
+public class ParentChildDriver {
+
+	public static void main(String[] args) {
+		
+//		GrandParent gp = new GrandParent();
+//		
+//		gp.getData();
+//		
+//		System.out.println("=======================");
+
+//		Parent p1 = new Parent();
+
+////	System.out.println(p1.a);
+//		p1.test();
+//		p1.getData();
+		
+////		
+//		System.out.println("=====================");
+//
+//		Child c1 = new Child();
+		
+////	System.out.println(c1.b);
+//		c1.demo();
+
+	}
+
+}
