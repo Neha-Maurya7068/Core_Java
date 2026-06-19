cc561b4492a1eccb83538b8e2d566fa50f2826b5
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-19 20:38:55 +0530

    Driver class

---
+++ OurArrayListDriver.java
@@ -0,0 +1,27 @@
+package our_list;
+
+public class OurArrayListDriver {
+
+	public static void main(String[] args) {
+
+		OurArrayList<Integer> list = new OurArrayList<Integer>();
+
+		list.add(10);
+		list.add(20);
+		list.add(30);
+
+		System.out.println(list);
+
+//		list.remove(1);
+
+//		System.out.println(list.remove(1));
+
+//		list.set(1, 50);
+
+//		System.out.println(list);
+
+//		System.out.println(list.get(4));
+
+	}
+
+}
