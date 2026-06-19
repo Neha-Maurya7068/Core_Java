5acbf3cd5ce7cb99a2e55f43b44e6637e0416a3b
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-19 23:35:22 +0530

    Add files via upload

---
+++ Question_7.java
@@ -0,0 +1,32 @@
+package assignment_generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Question_7 {
+
+	public static void main(String[] args) {
+
+//		List l1 = new ArrayList();
+//		
+//		l1.add(10);
+//		
+//		l1.add("Akansh");
+//		
+//		System.out.println(l1);
+
+		
+		
+//		List<Integer> l2 = new ArrayList<>();
+//
+//		l2.add(10);
+//		l2.add(20);
+//		l2.add(30);
+//
+////		l2.add("Ram");
+//
+//		System.out.println(l2);
+
+	}
+
+}
