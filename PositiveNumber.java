3ea325660ea518e25f1269393c5c63482481ea83
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-19 23:25:59 +0530

    Positive Number

---
+++ PositiveNumber.java
@@ -0,0 +1,48 @@
+  public class PositiveNumber{
+  
+  public static void main(String[]args){
+  
+  int n = 30;
+ 
+
+  if(n>0){
+    
+  System.out.println( n+ ":Is Positive Number!");
+    
+  if(n % 2 == 0){
+    
+  System.out.println( n+ ":Is Even Positive Number!");
+    
+  }
+  
+  else{
+    
+  System.out.println(n+ ":Is Odd Positive Number!");
+    
+  }
+  
+  }
+  else{
+    
+  System.out.println(n+ ":Is Negative Number");
+   
+   if(n % 2 == 0){
+    
+  System.out.println(n+ ": Is Even Negative Number");
+    
+  }
+ else {
+    
+  System.out.println(n+ "Is Odd Negative Number");
+    
+  }
+
+  }
+  
+
+ }
+
+ }
+
+
+   
