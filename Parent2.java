da30ab47d87b2d8dfc75569115839c7b2a0857a4
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-19 23:21:24 +0530

    Add files via upload

---
+++ Parent2.java
@@ -0,0 +1,12 @@
+package typecasting;
+
+public class Parent {
+
+	void eat() {
+		System.out.println("Eat From Parent Money !!");
+	}
+	
+	void earn() {
+		System.out.println("Parent Earn A Lot Of Money !!");
+	}
+}
