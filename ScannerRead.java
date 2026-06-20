4215cbfa7b1d5ec216fcc22002d97692c68a9c6f
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-20 21:47:42 +0530

    Scanner using read

---
+++ ScannerRead.java
@@ -0,0 +1,61 @@
+package file_handling;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class ScannerRead {
+
+	public static void main(String[] args) throws IOException {
+
+//		File file = new File("E:\\Deepanshu\\MyFolder\\Deep.txt");
+
+//		Scanner sc;
+//		try {
+//			sc = new Scanner(f1);
+//			String data = sc.nextLine();
+//
+//			System.out.println(data);
+//
+//			sc.close();
+//		} catch (FileNotFoundException e) {
+//			System.out.println("File Not Read !!");
+//		}/
+		
+	
+//		****************** hasNextLine() *********************************
+		
+//		File file = new File("E:\\Deepanshu\\MyFolder\\Deepanshu.txt");
+//	        Scanner sc = new Scanner(file);
+//
+//	        while (sc.hasNextLine()) {
+//	            System.out.println(sc.nextLine());
+//	        }
+//	        sc.close();
+		
+//		*************** Professional ************************************	
+
+		      
+		/*        
+		
+		        PrintWriter pw = new PrintWriter("E:\\Deepanshu\\MyFolder\\Deepanshu.txt");
+	
+		        pw.println("Deepanshu File");
+		        pw.println("File Handling preparation");
+		        pw.close();
+		        
+		 */       
+		
+		
+//		Modern Java ****************************
+
+//		          try (FileWriter fw = new FileWriter("E:\\Deepanshu\\MyFolder\\Deep.txt")) {
+//		                    fw.write("Modern Java");
+//		                }	            
+		    
+		
+	}
+
+}
