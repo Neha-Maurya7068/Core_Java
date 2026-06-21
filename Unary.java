eb208e2ddc9db954778a7496fdae0a24cf5562f1
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-21 20:13:54 +0530

    Unary Operator

---
+++ Unary.java
@@ -0,0 +1,34 @@
+public class Unary{
+  public static void main(String[]args){
+
+
+    //Unary Opertaor***********
+    //int x = 20;
+    //x++;
+    //int y = x++;
+    //int z = ++y;
+
+  //System.out.println(x);
+  //System.out.println(y);
+  //System.out.println(z);
+
+  // int x = 2;
+ //int Sum = x++ + x + x++ + ++x;
+ //System.out.println(Sum);
+
+  //int x = 2;
+  //int Sum = x++ + x++ + ++x;
+  //System.out.println(Sum);
+
+   int a = 70;
+   int Sum = ++a + --a + a + --a - --a + ++a;
+   System.out.println(Sum);
+
+
+
+  //Ternary Opertaor**********
+
+   
+  
+}
+}
