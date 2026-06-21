9d4dd0dc42ae1cbf2e8a942194026aa1774e37e6
Neha Maurya <er.nehamaurya7068@gmail.com>
2026-06-21 11:39:23 +0530

    Add files via upload

---
+++ Sum1.java
@@ -0,0 +1,19 @@
+public class Sum1{
+ 
+public static void main(String[]args){
+
+/Arithematic Operator
+    int a = 20;
+    int b = 30;
+    int sum = a + b;
+    int sub = a - b;
+    int mul = a * b;
+    int div = a / b;
+    int mod = a % b;
+System.out.println(sum);
+System.out.println(sub);
+System.out.println(mul);
+System.out.println(div);
+System.out.println(mod);
+}
+}
